9ee8fd61e381d1d5ea3213b8bffe964c4105f7fb
Marc Wright <marc.wright@gmail.com>
2019-11-05 10:20:49 -0500

    Updates

---
+++ precache-manifest.9675fab38b0a1bba4a543ace78d967e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a9c9687cd69fbb08189c4ad7a1ea343",
+    "url": "/trump-v-ye/index.html"
+  },
+  {
+    "revision": "7a14671df3a122ac62ab",
+    "url": "/trump-v-ye/static/css/main.67e249d6.chunk.css"
+  },
+  {
+    "revision": "a23ecc0089fabdcf5647",
+    "url": "/trump-v-ye/static/js/2.20e6b179.chunk.js"
+  },
+  {
+    "revision": "7a14671df3a122ac62ab",
+    "url": "/trump-v-ye/static/js/main.bca9f689.chunk.js"
+  },
+  {
+    "revision": "b1cb97529783b8de62d9",
+    "url": "/trump-v-ye/static/js/runtime-main.5b4cf019.js"
+  }
+]);
